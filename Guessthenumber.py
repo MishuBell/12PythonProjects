e43b5d2b7012583ebecec0391851aa41955ac298
Mishu Bell <mbosb@pm.me>
2025-05-20 17:34:37 +0200

    Project 2 of 12 PythonProjects

--- Guessthenumber.py
+++ Guessthenumber.py
@@ -1,15 +1,29 @@
 import random
 
-random_number = random.randint(0, 100)
+random_number = random.randint(0, 5)
 
 def guess():
-    user_input = input("Guess a number: ")
+    user_input = int(input("Guess a number: "))
 
     while user_input != random_number:
-        if int(user_input) < random_number:
-            user_input = input("Too low! Guess again: ")
-        if int(user_input) > random_number:
-            user_input = input("Too high! Guess again:")
-        if int(user_input) == random_number:
-            print("{user_input} is correct!".format())
+        if user_input < random_number:
+            user_input = int(input("Too low! Guess again: "))
+        if user_input > random_number:
+            user_input = int(input("Too high! Guess again:"))
+    print(f"{user_input} is correct!")
+
+
+def guess_2(x):
+    random_number = random.randint(1,x)
+    guess = 0
+
+    while guess != random_number:
+        guess = int(input(f"Guess a number between 1 and {x}"))
+        if guess < random_number:
+            print("Sorry! Guess higher")
+        elif guess > random_number:
+            print("Sorry. Guess lower ...")
+    print("Correct! JACKPOT!!")
+
 guess()
+guess_2(5)
