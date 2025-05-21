a7a5eb763dce44088c1fb7f03058ebf26fbf5119
Mishu Bell <mbosb@pm.me>
2025-05-21 19:13:15 +0200

    Halting the challenge to re-assess

---
+++ UserGuessNumber.py
@@ -0,0 +1,33 @@
+import random
+
+# We have a number in mind
+# The computer must guess the number
+
+# The computer asks in what range, from 1 to X
+# then it stores the numbers it guessed already
+# could use random or binary search
+
+# def computer_guess():
+#     print("Tutorial: First define a range by entering a number. Then when prompted, type 'Lower' or 'Higher' or 'YES'")
+
+#     wrong_numbers = []
+#     user_defined_range = int(input("Define a range from 1 to ... ?"))
+#     computer_guess = random.randint(1, user_defined_range)
+   
+
+#     # OR
+#     user_input = input("Prompt: ")
+#     low = 1
+#     high = user_defined_range
+
+#     while user_input != "YES":
+#         print(f"Is the number {computer_guess} ?")
+#         print(user_input)
+#         wrong_numbers.__add__(computer_guess)
+
+
+## STOP THIS AINT IT.
+## Lets think about it piece by piece
+
+
+
